9fc879e816261af4ac5f967c322e633d1ca71237
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-04 13:33:32 -0500

    Engine for database connection

--- simple.py
+++ simple.py
@@ -13,3 +13,5 @@ Run with:
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+# Connect to database
+engine = create_engine("postgresql://labuser:labpass@db:5432/sandbox")
