7b1fee62b21aaad771b611ee5c9fe7e5c261bf7b
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-04 13:32:02 -0500

    Dependencies

--- simple.py
+++ simple.py
@@ -8,3 +8,8 @@ Simplest SQLAlchemy use:
 Run with:
     python simple.py
 """
+
+# Import dependencies
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
