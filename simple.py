80b7e5f50993ea7cb54a15ee24ea64eecbe008ca
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-04 13:36:41 -0500

    Created User base for models, created a table, and session

--- simple.py
+++ simple.py
@@ -15,3 +15,20 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 # Connect to database
 engine = create_engine("postgresql://labuser:labpass@db:5432/sandbox")
+
+# Define a base for models
+Base = declarative_base()
+
+# Define a simple model
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+# Create the table
+Base.metadata.create_all(engine)
+
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
