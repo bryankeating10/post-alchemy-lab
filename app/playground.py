795ab37a4f9803220eae45c38559e09968fe7d2f
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-08 16:08:36 -0500

    Beginning SQLAlchemy exploration

--- app/playground.py
+++ app/playground.py
@@ -2,3 +2,14 @@
 Investigates SQLAlchemy and PostgreSQL interactions:
     - Connect to Postgres
 """
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+# Connection string for PostgreSQL
+DATABASE_URL = 'postgresql://labuser:labpass@db:5432/sandbox'
+
+# Create an engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+
