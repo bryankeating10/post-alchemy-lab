4a75daacf796f7fb9e8158635694ffb426567cb8
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-08 16:54:21 -0500

    Experimenting with basic SQLAlchemy schema creation
    Going to be using Rutgers Mens Soccer game data from 2022

--- app/playground.py
+++ app/playground.py
@@ -3,7 +3,7 @@ Investigates SQLAlchemy and PostgreSQL interactions:
     - Connect to Postgres
 """
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 # Connection string for PostgreSQL
@@ -12,4 +12,24 @@ DATABASE_URL = 'postgresql://labuser:labpass@db:5432/sandbox'
 # Create an engine
 engine = create_engine(DATABASE_URL, echo=True)
 
+# Define the ORM Base
+Base = declarative_base()
 
+class RUMS(Base):
+    __tablename__ = 'rums'
+
+    id = Column(Integer, primary_key=True)
+
+    # Match details
+    datetime = Column(DateTime)
+    location = Column(String)
+    
+    # Competitors
+    home_team = Column(String)
+    away_team = Column(String)
+
+    # Match statistics
+    home_goals = Column(Integer)
+    away_goals = Column(Integer)
+    home_shots = Column(Integer)
+    away_shots = Column(Integer)
