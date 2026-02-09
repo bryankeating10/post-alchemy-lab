3f03b51365423fda44b94d8961c4903258ba6c88
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-09 15:35:25 -0500

    Successfully downloaded walmart dataset using kaggle api

--- app/panda_kaggle.py
+++ app/panda_kaggle.py
@@ -7,15 +7,24 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 import pandas as pd
-
 from kaggle.api.kaggle_api_extended import KaggleApi
+
+from table_creation import Session, WalmartSales
 
 # Download dataset using Kaggle API
 api = KaggleApi()
 api.authenticate()
+print('Authenticated API')
 
 api.dataset_download_files(
     'mikhail1681/walmart-sales',
     path='/app/data',
     unzip=True
 )
+print('Downloaded sales data')
+
+df = pd.read_csv('/app/data/Walmart_Sales.csv')
+print(df.head())
+
+# Open a session
+session = Session()
