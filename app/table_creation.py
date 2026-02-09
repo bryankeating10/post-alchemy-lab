0a331b3f29d04823e784f45bb264c5fe812c0fa8
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-09 06:16:35 -0500

    Create tables for entire practice repo here

---
+++ app/table_creation.py
@@ -0,0 +1,12 @@
+"""
+Define the schema for all tables in the database
+"""
+
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+# Connection string for PostgreSQL
+DATABASE_URL = 'postgresql://labuser:labpass@db:5432/sandbox'
+
+# Create an engine
+engine = create_engine(DATABASE_URL, echo=False)
