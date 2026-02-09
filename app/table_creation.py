a81cb702a862fd05f48135afc158f7ca5afbd469
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-09 06:25:25 -0500

    RUMS Schema

--- app/table_creation.py
+++ app/table_creation.py
@@ -10,3 +10,26 @@ DATABASE_URL = 'postgresql://labuser:labpass@db:5432/sandbox'
 
 # Create an engine
 engine = create_engine(DATABASE_URL, echo=False)
+
+# Define the ORM Base
+Base = declarative_base()
+
+# Define the RUMS table
+class RUMS(Base):
+    __tablename__ = 'rums'
+    
+    id = Column(Integer, primary_key=True)
+
+    # Match details
+    datetime = Column(DateTime)
+    location = Column(String)
+    
+    # Competitors
+    home_team = Column(String)
+    away_team = Column(String)
+
+    # Match statistics
+    home_goals = Column(Integer)
+    away_goals = Column(Integer)
+    home_shots = Column(Integer)
+    away_shots = Column(Integer)
